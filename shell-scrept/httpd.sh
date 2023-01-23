19ae57d981a3909ad97994964d03c19a97346cf7
manishkumarpatel <manish080194@gmail.com>
2023-01-23 05:59:33 +0000

    shell scrept for monitoring httpd every 5 second

--- shell-scrept/httpd.sh
+++ shell-scrept/httpd.sh
@@ -1,20 +1,29 @@
 SHELL=/bin/bash
 PATH=/sbin:/bin:/usr/sbin:usr/bin
 
-
-
-
-
 #!/bin/bash
+
+
+while true
+do
 
 service httpd status > output.txt
 
-if grep -q "running" output.txt; then
-echo "the httpd service is running"
+if grep -q "running" output.txt; 
+then
+
+	echo "the httpd service is running"
 
 else
-service httpd start
-echo "The httpd service was stopped, errored, or inactive. The service has been started>"
+
+	service httpd start
+
+	echo "The httpd service was stopped, errored, or inactive. The service has been started>"
 fi
 
 sudo rm -f output.txt
+
+
+sleep 5s
+
+done
